db905697ac0cf6f770e6a3bc12fdd98556faa619
LM Train <layconmm@msn.com>
2020-04-17 17:54:49 -0500

    Run build for deployement

---
+++ client/build/precache-manifest.0a31a36bbc35969951ec9c4cd4483398.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b477ad0ad1acf9fcc28fc11dae3550",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46083cb5bc34d9db5823",
+    "url": "/static/css/2.465d1ef4.chunk.css"
+  },
+  {
+    "revision": "0e3b950c7aebf67812ab",
+    "url": "/static/css/main.e5442798.chunk.css"
+  },
+  {
+    "revision": "46083cb5bc34d9db5823",
+    "url": "/static/js/2.42c741a1.chunk.js"
+  },
+  {
+    "revision": "042f2b0c18596f1cdf6828b6d6a9f155",
+    "url": "/static/js/2.42c741a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e3b950c7aebf67812ab",
+    "url": "/static/js/main.90ae4b84.chunk.js"
+  },
+  {
+    "revision": "60c073e438479310a2bb",
+    "url": "/static/js/runtime-main.7dc8ba12.js"
+  }
+]);
